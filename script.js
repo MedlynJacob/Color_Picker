ed4f9178980c489b6824d40ecd04e41bbe895d32
MedlynJacob <88194420+MedlynJacob@users.noreply.github.com>
2021-08-22 10:24:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function make_red(){
+    document.body.style.backgroundColor="rgb(230, 16, 58)"
+    
+}
+function make_redorange(){
+    document.body.style.backgroundColor="rgb(235, 38, 38)"
+}
+function make_orange(){
+    document.body.style.backgroundColor="rgb(247, 123, 7)"
+}
+
+function make_green(){
+    document.body.style.backgroundColor="rgb(115, 177, 22"
+}
+function make_yelgre(){
+    document.body.style.backgroundColor="yellowgreen"
+}
+function make_yel(){
+    document.body.style.backgroundColor="rgb(208, 255, 115)"
+}
+function make_blue(){
+    document.body.style.backgroundColor="rgb(9, 9, 139)"
+}
+function make_indigo(){
+    document.body.style.backgroundColor="indigo"
+}
+function make_violet(){
+    document.body.style.backgroundColor="violet"
+}
